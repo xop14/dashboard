b10468db93a49393b94c879ae3ef5786de0e94ed
xop14 <112329100+xop14@users.noreply.github.com>
2022-12-05 22:13:35 +0900

    Add mobile layout and mobile menu. Adjust colors

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const themeSwitch = document.querySelector(".theme-switch");
 let currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : "light";
 const heartIcons = document.querySelectorAll(".heart-icon");
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
 
 // DARK MODE
 
@@ -18,14 +21,19 @@ themeSwitch.addEventListener("click", (e) => {
         currentTheme = "dark";
         localStorage.setItem("theme", "dark");
     }
-})
+});
 
 
 
-// HEART SELECTED
+// HEART SELECT
 
 heartIcons.forEach((heart) => {
     heart.addEventListener("click", (e) => {
         e.target.classList.toggle("heart-icon-selected");
     })
-})
+});
+
+
+
+// HAMBURGER MENU
+hamburger.addEventListener("click", () => navbar.classList.toggle("navbar-open"));
